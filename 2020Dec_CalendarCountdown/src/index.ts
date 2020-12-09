653d398601ad96986070f06e1861e2c6d6465194
panvicka <panvicka@gmail.com>
2020-12-09 20:56:57 +0100

    countdown working

--- 2020Dec_CalendarCountdown/src/index.ts
+++ 2020Dec_CalendarCountdown/src/index.ts
@@ -1,1 +1,28 @@
-console.log("works");
+const span = <HTMLSpanElement>document.getElementById("count-down-number")!;
+const divToAnimate = <HTMLDivElement>document.getElementById("firework")!;
+const fireworkHolder = <HTMLDivElement>(
+  document.querySelector(".animation-holder")!
+);
+
+const START_VALUE = 10;
+
+let number = START_VALUE;
+
+window.setInterval(function () {
+  if (number > 0) {
+    const newFireWork = document.createElement("div");
+    newFireWork.id = "firework";
+    newFireWork.classList.add("animate");
+    fireworkHolder.appendChild(newFireWork);
+    number = number - 1;
+    span.innerText = number.toString();
+  } else {
+    span.innerText = "Click to restart";
+    span.classList.add("restart-text");
+    span.addEventListener("click", () => {
+    number = START_VALUE;
+    span.innerText = "";
+    span.classList.remove("restart-text");
+    });
+  }
+}, 1500);
