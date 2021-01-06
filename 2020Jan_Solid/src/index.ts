513d26f8e4b0c91d39928bf03c454d5a4e33aba0
panvicka <panvicka@gmail.com>
2021-01-06 20:22:28 +0100

    sliders are moving

--- 2020Jan_Solid/src/index.ts
+++ 2020Jan_Solid/src/index.ts
@@ -1,19 +1,22 @@
 console.log('hesdy');
-const containerDiv = <HTMLDivElement>document.getElementById('container');
-const templateSlice = <HTMLElement>document.getElementById('slice');
+// const containerDiv = <HTMLDivElement>document.getElementById('container');
+// const templateSlice = <HTMLElement>document.getElementById('slice-template');
 
 class SlideComponent<T extends HTMLElement, U extends HTMLElement> {
   templateElement: HTMLTemplateElement;
   hostElement: T;
   element: U;
   text: string;
-
+  startPosition: number;
+  moved: boolean;
   constructor(
     templateId: string,
     hostElementId: string,
     startPosition: number,
     text: string
   ) {
+    this.moved = true;
+    this.startPosition = startPosition;
     this.text = text;
     this.templateElement = <HTMLTemplateElement>(
       document.getElementById(templateId)!
@@ -38,12 +41,34 @@ class SlideComponent<T extends HTMLElement, U extends HTMLElement> {
 
   render() {
     this.element.querySelector('#text')!.textContent = this.text.toString();
+    const sliderElement = <HTMLDivElement>(
+      this.element.querySelector('#slider')!
+    );
+    const textElement = <HTMLDivElement>this.element.querySelector('#text')!;
+    sliderElement.style.transform = `translateY(-50%) translateX(${this.startPosition}rem)`;
+    textElement.style.transform = `translateX(${this.startPosition}rem)`;
+
+    sliderElement.addEventListener('click', () => {
+      let moveOffset;
+      if (this.moved) {
+        moveOffset = '0px';
+        this.moved = false;
+      } else {
+        moveOffset = `${this.startPosition}rem`;
+        this.moved = true;
+
+      }
+      console.log(this.moved);      
+      console.log(moveOffset);
+      
+      sliderElement.style.transform = `translateY(-50%) translateX(${moveOffset})`;
+
+     });
   }
 }
 
-const slideS = new SlideComponent('slice', 'container', 10, 'S');
- const slideL = new SlideComponent('slice', 'container', 10, 'O');
- const slideI = new SlideComponent('slice', 'container', 10, 'L');
- const slideD = new SlideComponent('slice', 'container', 10, 'I');
- const slideE = new SlideComponent('slice', 'container', 10, 'D');
- 
+const slideS = new SlideComponent('template-slice', 'container', 15, 'S');
+const slideL = new SlideComponent('template-slice', 'container', -16, 'O');
+const slideI = new SlideComponent('template-slice', 'container', 13, 'L');
+const slideD = new SlideComponent('template-slice', 'container', -13, 'I');
+const slideE = new SlideComponent('template-slice', 'container',16, 'D');
