c1e3a6e3d2ee9a7333f3c6185b0215c166a57e1e
panvicka <panvicka@gmail.com>
2021-09-14 20:12:57 +0200

    started files spotlight

---
+++ 2021Sep_Spotlight/index.js
@@ -0,0 +1,28 @@
+// // gsap.from('.light', { duration: 2, y: '-100%', ease: 'bounce' });
+//  gsap.from('.red', {
+//   duration: 3,
+// //   rotation: -180,
+//   rotationZ:360,
+//   transformOrigin: 'bottom 50%',
+
+// });
+
+// gsap.from('.blue', {
+//   duration: 3,
+//   rotation: 180,
+//   transformOrigin: 'bottom 50%',
+// });
+
+// gsap
+//   .timeline({
+//     repeat: -1,
+//     defaults: {
+//       transformOrigin: 'bottom 50%',
+//       duration: 1,
+//       ease: 'expo.inOut',
+//     },
+//   })
+//   .to('.blue', { rotateX: 0, rotateZ: 0 })
+//   .to('.blue', { rotateX: 0, rotateZ: -50 })
+//   .to('.blue', { rotateX: 0, rotateZ: 70 })
+//   .to('.blue', { rotateX: 0, rotateZ: 0 });
