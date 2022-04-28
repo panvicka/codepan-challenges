d38ecc965f1cc6721958edf5a57b5fa671fe4f4d
panvicka <panvicka@gmail.com>
2022-04-28 19:31:27 +0200

    finished birds

--- 2022/2022_05_Week_4_Article/index.js
+++ 2022/2022_05_Week_4_Article/index.js
@@ -3,6 +3,7 @@ window.addEventListener("resize", () => {
 });
 
 window.addEventListener("load", () => {
+  document.getElementById("hideAll").style.display = "none";
   setPosition();
 });
 
@@ -10,9 +11,19 @@ const content = document.querySelector(".content");
 const sidePanel = document.querySelector(".side");
 const image = document.querySelector("img");
 const images = document.querySelectorAll("img");
-
+const like = document.getElementById("like");
+const bookmark = document.getElementById("bookmark");
 
 const NUMBER_OF_IMAGES = 5;
+let scrollHeight = 0;
+
+like.addEventListener("click", () => {
+  like.classList.toggle("like-active");
+});
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark-active");
+});
 
 document.addEventListener("scroll", (e) => {
   var scrollTop =
@@ -20,14 +31,10 @@ document.addEventListener("scroll", (e) => {
       ? window.pageYOffset
       : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
-  console.log(scrollTop, image.clientHeight);
-
-  const boundForScrolling = content.scrollHeight / NUMBER_OF_IMAGES+1;
+  const boundForScrolling = content.scrollHeight / NUMBER_OF_IMAGES + 1;
   const imageIndex = Math.floor(scrollTop / boundForScrolling) + 1;
 
   const imageToShow = document.getElementById(`img-${imageIndex}`);
-  console.log(`image to show with index ${imageIndex}`);
-  console.log(imageToShow);
   imageToShow.style.opacity = 1;
 
   for (let i = 1; i <= NUMBER_OF_IMAGES; i++) {
@@ -38,24 +45,15 @@ document.addEventListener("scroll", (e) => {
   }
 });
 
-let scrollHeight = 0;
-
 const setPosition = () => {
   const sidePanelBoundaries = sidePanel.getBoundingClientRect();
-
   content.style.marginLeft = `${sidePanelBoundaries.width}px +10em`;
   scrollHeight = content.scrollHeight;
-  console.log(scrollHeight);
-
   const imageBoundaries = image.getBoundingClientRect();
-
   sidePanel.style.width = imageBoundaries.width + "px";
-  // sidePanel.style.marginLeft = imageBoundaries.x + 'px';
-//   image.style.left = sidePanelBoundaries.x + "px";
 
-  images.forEach(image => {
-      image.style.left = sidePanelBoundaries.x + "px";
-  })
-
-  console.log(imageBoundaries);
+  images.forEach((image) => {
+    image.style.left = sidePanelBoundaries.x + "px";
+    image.style.width = imageBoundaries.width + "px";
+  });
 };
